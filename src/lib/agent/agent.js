8ebc3da2d6b576b0f89f551c535d4c551a3784db
Yoshiyuki Kato <yoshiyuki_kato@r.recruit.co.jp>
2017-04-19 18:22:02 +0900

    update

---
+++ src/lib/agent/agent.js
@@ -0,0 +1,108 @@
+import Promise from "bluebird";
+import $ from "jquery"; //jqueryはexternalにしてビルドする
+import User from "./user";
+import Service from "./service";
+
+export default class Agent{
+  constructor({ env, api, codebase }, url=""){
+    this.env = env; //dev|prod
+    this.api = api; //api-gatewayのurl
+    this.codebase = codebase; //s3のurl
+    this.url = url; //サイトのurl
+    this.services = [];
+    this.user = null;
+  }
+
+  init(){
+    //1. 描画インタフェース関数のエクスポート
+    this.exportRender();
+    //2. サービスのロード & ユーザデータのロード
+    const servicesPromise = this.getServices();
+    const userDataPromise = this.getUserData();
+    Promise.all(servicesPromise, userDataPromise)
+    .then((result) => {
+      this.services = result[0];
+      this.user = result[1];
+    });
+  }
+
+  //サービスのリストを全部取ってきて、URLのパターンで絞り込む
+  getServices(){
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        type : "GET",
+        url : `${this.api}/services`,
+        dataType : "json",
+        success : (res) => { resolve(res.services); },
+        error : (err) => { reject(new Error(err.message)); }
+      });
+    })
+    .filter((service) => {
+      //パターンが未定義なら除外
+      if(!service.pattern) return false;
+      //urlにマッチするサービスのみ抽出
+      const regexp = new RegExp(pattern);
+      return !!this.url.match(regexp);
+    })
+    .reduce((services, service) => {
+      //idで引けるようにして
+      services[service.id] = new Service(service);
+      return services;
+    }, [])
+    .map((service) => {
+      service.load(this.codebase);
+    });
+  }
+
+  getUserData(){
+    //APIのリストを取得
+    return this._loadDataList
+    //一つずつユーザのデータを読みに行って、ひとまとめにする
+    .map(this._loadData).reduce((userData, data)=>{
+      userData[data.name] = data;
+      return userData;
+    }, this.userData)
+    .then((userData) => {
+      return new User(userData);
+    });
+  }
+
+  _getDataSourceList(){
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        type : "GET",
+        url : `${this.api}/data_sources`,
+        dataType : "json",
+        success : (res) => { resolve(res.dataSources); },
+        error : (err) => { reject(new Error(err.message)); }
+      });
+    });
+  }
+
+  //ユーザデータのapi読みに行く
+  _getData({ name, url, key_id }){
+    return new Promise((resolve, reject) => {
+      if(!this.user[key_id]) resolve({});
+      $.ajax({
+        type : "GET",
+        url : `${url}?${key_id}=${this.user[key_id]}`,
+        dataType : "json",
+        success : (res) => {
+          const result = {};
+          result[name] = res;
+          resolve(result);
+        },
+        error : (err) => { reject(new Error(err.message)); }
+      });
+    });
+  }
+
+  exportProvide(){
+    window[`__gizmo_provide_${this.env}__`] = this.provideService.bind(this);
+  }
+
+  provideService(id, adapt, render){
+    const service = this.services[id];
+    if(!!service) service.init(this.user, adapt, render);
+  }
+}
--- src/lib/wrapper.js
+++
@@ -1,9 +0,0 @@
-function wrap(id, script, env){
-  const body = `__grender_${env}__(${id}, function(){${script}});`
-  return s3.putObject({
-    Bucket : "gizmo-assets",
-    Key : `public/${event.item.id}.js`,
-    Body : body,
-    CacheControl : "no-cache, no-store"
-  }).promise();
-}

52d7b109bd1c4ba52e46a1ec785509b4f63c2d41
YoshiyukiKato <yo.shuuuuki@gmail.com>
2017-06-05 04:52:05 +0900

    add modules

---
+++ src/ts/gizmo-items/index.ts
@@ -0,0 +1,42 @@
+import App from "../app";
+import User from "../user";
+
+export default class GizmoItems{
+  private itemMap: Map<string, GizmoItem> = new Map<string, GizmoItem>();
+  private itemList: GizmoItem[] = [];
+  
+  create(item:{id:string, src:string}){
+    const newItem = new GizmoItem(item);
+    this.itemMap[item.id] = newItem;
+    this.itemList.push(newItem);
+    newItem.init();
+    return newItem;
+  }
+
+  all(){
+    return this.itemList;
+  }
+
+  find(id:string){
+    return this.itemMap[id];
+  }
+}
+
+class GizmoItem{
+  private id:string;
+  private src:string;
+  constructor(item:{id:string, src:string}){
+    this.id = item.id;
+    this.src = item.src;
+  }
+
+  init(){
+    const script = document.createElement("script");
+    script.id = this.id;
+    script.src = this.src;
+  }
+
+  onChangeUser(user:User, render:Function){
+    render(user);
+  }
+}
